50a479a9f849970ef8df09e660d2b471fcd88048
ainur <ainur@mail.ru>
2022-09-03 14:03:28 +0300

    coomit

--- class.php
+++ class.php
@@ -24,12 +24,46 @@ class BootstrapStarRating extends \CBitrixComponent
 		$ELEMENT_ID = $PARAMS["ELEMENT_ID"];
 		if($ELEMENT_ID>0)
 		{
+			$obProperty = new CIBlockProperty;
 			$voteСount = $currentRating = $voteSum = 0; 
 			$rsProperties = CIBlockElement::GetList([], ["IBLOCK_ID" => $PARAMS["IBLOCK_ID"], "ID" => $ELEMENT_ID], false, ['nTopCount' => 1], ['ID', "IBLOCK_ID", "PROPERTY_vote_count", "PROPERTY_rating", "PROPERTY_vote_sum"]);
 			if($arProperty = $rsProperties->Fetch())
 			{
+				if($arProperty['PROPERTY_VOTE_COUNT_VALUE'] === null){
+					$res = $obProperty->Add(array(
+						"IBLOCK_ID" => $PARAMS["IBLOCK_ID"],
+						"ACTIVE" => "Y",
+						"PROPERTY_TYPE" => "N",
+						"MULTIPLE" => "N",
+						"NAME" => GetMessage("CC_BIV_VOTE_COUNT"),
+						"CODE" => "vote_count",
+					));
+
+				}
 				$voteСount = (int)$arProperty['PROPERTY_VOTE_COUNT_VALUE'];
+
+				if($arProperty['PROPERTY_RATING_VALUE'] === null){
+					$res = $obProperty->Add(array(
+						"IBLOCK_ID" => $PARAMS["IBLOCK_ID"],
+						"ACTIVE" => "Y",
+						"PROPERTY_TYPE" => "N",
+						"MULTIPLE" => "N",
+						"NAME" => GetMessage("CC_BIV_VOTE_COUNT"),
+						"CODE" => "rating",
+					));
+				}
 				$currentRating = (float)$arProperty['PROPERTY_RATING_VALUE'];
+
+				if($arProperty['PROPERTY_VOTE_SUM_VALUE'] === null){
+					$res = $obProperty->Add(array(
+						"IBLOCK_ID" => $PARAMS["IBLOCK_ID"],
+						"ACTIVE" => "Y",
+						"PROPERTY_TYPE" => "N",
+						"MULTIPLE" => "N",
+						"NAME" => GetMessage("CC_BIV_VOTE_COUNT"),
+						"CODE" => "vote_sum",
+					));
+				}
 				$voteSum = (int)$arProperty['PROPERTY_VOTE_SUM_VALUE'];
 			}
 		}
---
+++ lang/ru/class.php
@@ -0,0 +1,4 @@
+<?
+$MESS["CC_BIV_VOTE_COUNT"] = "Количество проголосовавших";
+$MESS["CC_BIV_VOTE_SUM"] = "Сумма оценок";
+$MESS["CC_BIV_VOTE_RATING"] = "Рейтинг";
